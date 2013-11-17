27459574ef8166bf996c2b98ae0454946957dce7
William G Hatch <willghatch@gmail.com>
2013-11-16 18:26:12 -0700

    Add statement.c and statement.h that apparently I didn't commit earlier

---
+++ statement.c
@@ -0,0 +1,188 @@
+// statement.c
+
+#include <stdlib.h>
+#include "statement.h"
+#include "mipsout.h"
+
+conditional* mkConditional(expr* cond, BranchType btype, htslist* stmts) {
+    conditional* c = malloc(sizeof(conditional));
+    c->condition = cond;
+    c->branchType = btype;
+    c->statements = stmts;
+    return c;
+    // TODO - should I care about leaking memory with these things?  I haven't cared much so far.
+    // Frankly, all these tree nodes are just going to pile up until I have the full syntax tree,
+    // then all be written out and the process will exit... so it shouldn't matter.
+}
+
+statement* mkIfStmt(htslist* conditionals) {
+    statement* s = malloc(sizeof(statement));
+    s->type = if_stmt;
+    s->data.if_conditionals = conditionals;
+    return s;
+}
+
+statement* mkAssignmentStmt(expr* lvalue, expr* rvalue) {
+    statement* s = malloc(sizeof(statement));
+    s->type = assign_stmt;
+    s->data.assigndata.lvalue = lvalue;
+    s->data.assigndata.rvalue = rvalue;
+    return s;
+}
+
+statement* mkIncrementStmt(expr* lvalue, inctype type) {
+    openum op;
+    if(type == Increment) {
+        op = op_add;
+    } else if (type == Decrement) {
+        op = op_sub;
+    } else {
+        return NULL;
+    }
+    expr* rvalue = newBinOpExpr(op, lvalue, one_expr);
+    return mkAssignmentStmt(lvalue, rvalue);
+}
+
+statement* mkReadStmt(slist* exprList) {
+    statement* s = malloc(sizeof(statement));
+    s->type = read_stmt;
+    s->data.exprList = exprList;
+    return s;
+}
+
+statement* mkWriteStmt(slist* exprList) {
+    statement* s = malloc(sizeof(statement));
+    s->type = write_stmt;
+    s->data.exprList = exprList;
+    return s;
+}
+
+statement* mkLoopStmt(statement* init, conditional* cond, stmt_type looptype) {
+    statement* s = malloc(sizeof(statement));
+    s->type = looptype;
+    s->data.loopdata.init = init;
+    s->data.loopdata.cBlock = cond;
+    return s;
+}
+
+statement* mkForStmt(statement* assign, inctype updown, expr* finalVal, htslist* stmts) {
+    expr* lval = assign->data.assigndata.lvalue;
+    statement* update = mkIncrementStmt(lval, updown);
+    openum op = updown == Increment ? op_gt : op_lt;
+    expr* condition = newBinOpExpr(op, lval, finalVal);
+    conditional* cond = mkConditional(condition, bt_equal0, stmts);
+    hts_append(cond->statements, update);
+    statement* s = mkLoopStmt(assign, cond, for_stmt);
+    return s;
+}
+
+statement* mkRepeatStmt(expr* condition, htslist* stmts) {
+    conditional* c = mkConditional(condition, bt_equal0, stmts);
+    return mkLoopStmt(NULL, c, repeat_stmt);
+}
+
+statement* mkWhileStmt(expr* condition, htslist* stmts) {
+    conditional* c = mkConditional(condition, bt_nequal0, stmts);
+    return mkLoopStmt(NULL, c, while_stmt);
+}
+
+statement* mkStopStmt() {
+    statement* s = malloc(sizeof(statement));
+    s->type = stop_stmt;
+    return s;
+}
+
+
+//// Statement Evaluation:
+void stmt_eval_assign(statement* s) {
+    m_assign_stmt(s->data.assigndata.lvalue, s->data.assigndata.rvalue);
+}
+
+void stmt_eval_if(statement* s) {
+    m_if_stmt(s->data.if_conditionals);
+}
+
+void stmt_eval_while(statement* s) {
+    m_while_stmt(s->data.loopdata.cBlock);
+}
+
+void stmt_eval_repeat(statement* s) {
+    m_repeat_stmt(s->data.loopdata.cBlock);
+}
+
+void stmt_eval_for(statement* s) {
+    m_for_stmt(s->data.loopdata.init, s->data.loopdata.cBlock);
+}
+
+void stmt_eval_stop(statement* s) {
+}
+
+void stmt_eval_return(statement* s) {
+}
+
+void stmt_eval_read(statement* s) {
+    slist* lvalExprs = s->data.exprList;
+    m_read_stmt(lvalExprs);
+}
+
+void stmt_eval_write(statement* s) {
+    slist* lvalExprs = s->data.exprList;
+    m_write_stmt(lvalExprs);
+}
+
+void stmt_eval_proc(statement* s) {
+}
+
+void stmt_eval(statement* s) {
+    if (s == NULL) {
+        return;
+    }
+    stmt_type t = s->type;
+    switch(t) {
+        case assign_stmt:
+            stmt_eval_assign(s);
+            return;
+        case if_stmt:
+            stmt_eval_if(s);
+            return;
+        case while_stmt:
+            stmt_eval_while(s);
+            return;
+        case repeat_stmt:
+            stmt_eval_repeat(s);
+            return;
+        case for_stmt:
+            stmt_eval_for(s);
+            return;
+        case stop_stmt:
+            stmt_eval_stop(s);
+            return;
+        case return_stmt:
+            stmt_eval_return(s);
+            return;
+        case read_stmt:
+            stmt_eval_read(s);
+            return;
+        case write_stmt:
+            stmt_eval_write(s);
+            return;
+        case proc_stmt:
+            stmt_eval_proc(s);
+            return;
+        default:
+            return;
+    }
+}
+
+void eval_stmt_list(htslist* stmts) {
+    if (stmts == NULL) {
+        return;
+    }
+    slist* l = stmts->head;
+    while(l != NULL) {
+        stmt_eval(l->data);
+        l = l->next;
+    }
+    // TODO - I can probably free the list as well as the nodes here... If I decide I care about that.
+}
+
---
+++ statement.h
@@ -0,0 +1,72 @@
+// statement.h
+
+#ifndef STATEMENT_H
+#define STATEMENT_H
+
+#include "expression.h"
+#include "slist.h"
+
+// Increment type enum for loops
+typedef enum inctype_enum inctype;
+enum inctype_enum {
+    Increment, Decrement, Nocrement // Nocrement is for no-op
+};
+
+typedef enum stmt_type_enum stmt_type;
+enum stmt_type_enum {
+    assign_stmt, if_stmt, while_stmt, repeat_stmt, for_stmt, 
+    stop_stmt, return_stmt, read_stmt, write_stmt, proc_stmt, 
+    null_stmt
+};
+
+typedef enum BranchType_enum BranchType;
+enum BranchType_enum {
+    bt_equal0, bt_nequal0
+};
+
+typedef struct conditional_struct conditional;
+struct conditional_struct {
+    // a null condition will mean no condition.
+    expr* condition;
+    BranchType branchType;
+    htslist* statements;
+};
+
+typedef struct stmt_struct statement;
+struct stmt_struct {
+    stmt_type type;
+    union {
+        htslist* if_conditionals;
+        slist* exprList; // for read/write stmts
+        struct {
+            statement* init;
+            conditional* cBlock;
+        } loopdata;
+        struct {
+            expr* lvalue;
+            expr* rvalue;
+        } assigndata;
+    } data;
+};
+
+
+
+// Functions
+conditional* mkConditional(expr* cond, BranchType btype, htslist* stmts);
+statement* mkIfStmt(htslist* conditionals);
+statement* mkAssignmentStmt(expr* lvalue, expr* rvalue);
+statement* mkIncrementStmt(expr* lvalue, inctype type);
+statement* mkReadStmt(slist* exprList);
+statement* mkWriteStmt(slist* exprList);
+statement* mkLoopStmt(statement* init, conditional* cond, stmt_type looptype);
+statement* mkForStmt(statement* assign, inctype updown, expr* finalVal, htslist* stmts);
+statement* mkRepeatStmt(expr* condition, htslist* stmts);
+statement* mkWhileStmt(expr* condition, htslist* stmts);
+statement* mkStopStmt();
+void stmt_eval(statement* s);
+void eval_stmt_list(htslist* stmts);
+
+
+
+#endif //STATEMENT_H
+
